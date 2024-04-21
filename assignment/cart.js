6dfe2c124c8b9cad05dab177e043fd64d53fb51a
Justin Kahn <71735000+justinthegithub@users.noreply.github.com>
2024-04-21 13:55:14 -0500

    passing all tests

---
+++ assignment/cart.js
@@ -0,0 +1,63 @@
+console.log('***** Cart Functions *****');
+// Make sure to test all functions here in the JS file!
+// We want to see how you are testing your code!!!
+
+
+
+//- Create a global variable named `basket` and set it to an empty array.
+let basket = [];
+
+
+
+//Create a function called `addItem`. It should:
+//take an input parameter for a string `item`
+//add the new item to the global array `basket`.
+//return `true` indicating the item was added
+
+function addItem(item) {
+    basket.push(item)
+    return true;
+}
+
+
+
+
+//Create a function called `listItems`. It should:
+//loop over the items in the `basket` array
+//console.log each individual item on a new line
+function listItems(contents) {
+    for (let i = 0; i<contents.length;i++) {
+        console.log(contents[i]);
+    }
+
+}
+
+
+
+//Create a function called `empty`. It should:
+//reset the `basket` to an empty array
+// do not use `basket = []` to reset the array
+
+function empty(contents) {
+   return basket.splice(0, basket.length)
+}
+
+
+
+
+
+// DO NOT MODIFY
+// Used for automated testing
+try {
+    module.exports = {
+        basket: typeof basket !== 'undefined' ? basket : undefined,
+        addItem: typeof addItem !== 'undefined' ? addItem : undefined,
+        listItems: typeof listItems !== 'undefined' ? listItems : undefined,
+        maxItems: typeof maxItems !== 'undefined' ? maxItems : undefined,
+        empty: typeof empty !== 'undefined' ? empty : undefined,
+        isFull: typeof isFull !== 'undefined' ? isFull : undefined,
+        removeItem: typeof removeItem !== 'undefined' ? removeItem : undefined,
+    };
+} catch(e) {
+    // Do nothing
+}
