1beaf5f09a9b63ee828aa1049d8b867995693a4b
Justin Kahn <71735000+justinthegithub@users.noreply.github.com>
2024-04-21 13:56:04 -0500

    Add files via upload

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,9 +4,43 @@ console.log('***** Cart Functions *****');
 
 
 
+//- Create a global variable named `basket` and set it to an empty array.
+let basket = [];
 
 
 
+//Create a function called `addItem`. It should:
+//take an input parameter for a string `item`
+//add the new item to the global array `basket`.
+//return `true` indicating the item was added
+
+function addItem(item) {
+    basket.push(item)
+    return true;
+}
+
+
+
+
+//Create a function called `listItems`. It should:
+//loop over the items in the `basket` array
+//console.log each individual item on a new line
+function listItems(contents) {
+    for (let i = 0; i<contents.length;i++) {
+        console.log(contents[i]);
+    }
+
+}
+
+
+
+//Create a function called `empty`. It should:
+//reset the `basket` to an empty array
+// do not use `basket = []` to reset the array
+
+function empty(contents) {
+   return basket.splice(0, basket.length)
+}
 
 
 
